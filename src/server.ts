45370a133487a4b1e766beebfd0ee4b333a63eb8
Nisar Khan <khannisarnk0786@gmail.com>
2025-01-16 17:58:22 +0530

    working on register controller

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,17 @@ import os from 'os';
 import express from 'express';
 import bodyParser from 'body-parser';
 import rateLimit from 'express-rate-limit';
+import morgan from 'morgan';
+import dotenv from 'dotenv'
+dotenv.config();
+
+const port = process.env.PORT || 5001; // port 
+const numCPUs = os.cpus().length; //Number of CPUs cores
+
+// Global Rate Limiter
+const globalratelimiter = rateLimit({
+   windowMs: 15 * 60 * 1000, // 15 Minutes
+   max: 100, // Limit each IP to 100 requests per window
+   message: "Too Many requests from this IP, Please try again later."
+});
+
